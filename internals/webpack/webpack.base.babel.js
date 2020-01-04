08c8205cfb6a44937dff6ba009dc074e4f64136e
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-01-04 00:46:02 -0600

    teestcommit

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -12,7 +12,7 @@ module.exports = options => ({
     {
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
-      publicPath: '/',
+      publicPath: '/test-gh-pages-app',
     },
     options.output,
   ), // Merge with env dependent settings
